c71fe83532d185f8960977dd2602c9329537f4d3
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2022-07-08 03:28:19 -0700

    asdf

--- runelite-plugin/src/main/java/github/dqw4w9wgxcq/botapi/runeliteplugin/BotPlugin.java
+++ runelite-plugin/src/main/java/github/dqw4w9wgxcq/botapi/runeliteplugin/BotPlugin.java
@@ -19,71 +19,64 @@ import net.runelite.client.util.ImageUtil;
 import javax.inject.Inject;
 
 @PluginDescriptor(
-	name = "bot"
+        name = "bot"
 )
 @Slf4j
 /*
 I HAVE NOT TESTED IF THIS WORKS OR EVEN COMPILES CORRECTLY
  */
-public class BotPlugin extends Plugin
-{
-	@Inject
-	private Client client;
-	@Inject
-	private ClientThread clientThread;
-	@Inject
-	private EventBus eventBus;
+public class BotPlugin extends Plugin {
+    @Inject
+    private Client client;
+    @Inject
+    private ClientThread clientThread;
+    @Inject
+    private EventBus eventBus;
 
-	@Inject
-	private ClientToolbar clientToolbar;
+    @Inject
+    private ClientToolbar clientToolbar;
 
-	@Inject
-	private OverlayManager overlayManager;
+    @Inject
+    private OverlayManager overlayManager;
 
-	private NavigationButton navButton;
+    private NavigationButton navButton;
 //	private BotOverlay overlay;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		BotApiContext.INSTANCE.initialize(client, clientThread, eventBus);
-		ScriptManager scriptManager = new ScriptManager();
+    @Override
+    protected void startUp() throws Exception {
+        BotApiContext.INSTANCE.initialize(client, clientThread, eventBus);
+        ScriptManager scriptManager = new ScriptManager();
 
-		String scriptName = System.getProperty("bot.script");
-		if (scriptName != null)
-		{
-			startScript(scriptManager, scriptName);
-		}
+        String scriptName = System.getProperty("bot.script");
+        if (scriptName != null) {
+            startScript(scriptManager, scriptName);
+        }
 
-		navButton = NavigationButton.builder()
-			.tooltip("Bot")
-			.icon(ImageUtil.loadImageResource(ConfigPlugin.class, "config_icon.png"))
-			.priority(-420)
-			.panel(new BotPanel(scriptManager))
-			.build();
+        navButton = NavigationButton.builder()
+                .tooltip("Bot")
+                .icon(ImageUtil.loadImageResource(ConfigPlugin.class, "config_icon.png"))
+                .priority(-420)
+                .panel(new BotPanel(scriptManager))
+                .build();
 
-		clientToolbar.addNavigation(navButton);
+        clientToolbar.addNavigation(navButton);
 //		overlayManager.add(overlay = new BotOverlay());
-	}
+    }
 
-	private void startScript(ScriptManager scriptManager, String scriptName)
-	{
-		for (Class<? extends IBotScript> scriptClass : scriptManager.loadScripts())
-		{
-			if (scriptClass.getAnnotation(ScriptMeta.class).value().equalsIgnoreCase(scriptName))
-			{
-				scriptManager.startScript(scriptClass);
-				return;
-			}
-		}
+    private void startScript(ScriptManager scriptManager, String scriptName) {
+        for (Class<? extends IBotScript> scriptClass : scriptManager.loadScripts()) {
+            if (scriptClass.getAnnotation(ScriptMeta.class).value().equalsIgnoreCase(scriptName)) {
+                scriptManager.startScript(scriptClass);
+                return;
+            }
+        }
 
-		log.warn("cant find script: " + scriptName);
-	}
+        log.warn("cant find script: " + scriptName);
+    }
 
-	@Override
-	protected void shutDown()
-	{
-		clientToolbar.removeNavigation(navButton);
+    @Override
+    protected void shutDown() {
+        clientToolbar.removeNavigation(navButton);
 //		overlayManager.remove(overlay);
-	}
+    }
 }
--- runelite-plugin/src/main/java/github/dqw4w9wgxcq/botapi/runeliteplugin/TrimmingJTextArea.java
+++ runelite-plugin/src/main/java/github/dqw4w9wgxcq/botapi/runeliteplugin/TrimmingJTextArea.java
@@ -2,17 +2,14 @@ package github.dqw4w9wgxcq.botapi.runeliteplugin;
 
 import javax.swing.JTextArea;
 
-public class TrimmingJTextArea extends JTextArea
-{
-	@Override
-	public void append(String str)
-	{
-		String text = getText();
-		if (text.length() > 50000)
-		{
-			setText(text.substring(text.length() - 5000));
-		}
-		super.append(str);
-		setCaretPosition(getText().length());
-	}
+public class TrimmingJTextArea extends JTextArea {
+    @Override
+    public void append(String str) {
+        String text = getText();
+        if (text.length() > 50000) {
+            setText(text.substring(text.length() - 5000));
+        }
+        super.append(str);
+        setCaretPosition(getText().length());
+    }
 }

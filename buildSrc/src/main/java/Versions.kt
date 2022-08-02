836e48a509efe8b0b10f024289ddf0b569b726b5
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2022-08-02 10:23:57 -0700

    asdf

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val runelite = "1.8.26"
+    const val runelite = "1.8.30-SNAPSHOT"
     const val projectSubVersion = "a"
     const val project = "$runelite-$projectSubVersion"
     const val kotlin = "1.7.0"

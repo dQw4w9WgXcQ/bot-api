e0fc092bb622072c6f63e33448a47d9951786c14
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2022-09-22 10:17:46 -0700

    asdf

--- loader/src/main/java/github/dqw4w9wgxcq/botapi/loader/RuneliteContext.java
+++ loader/src/main/java/github/dqw4w9wgxcq/botapi/loader/RuneliteContext.java
@@ -1,21 +1,39 @@
 package github.dqw4w9wgxcq.botapi.loader;
 
-import lombok.Setter;
 import net.runelite.api.Client;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.eventbus.EventBus;
 
+import javax.inject.Inject;
 import java.io.File;
 
 public class RuneliteContext {
     public static final File dir = new File(RealUserHome.getUserHome(), "runelite-bot");
 
-    @Setter
+    @Inject
+    private Client client;
+    @Inject
+    private ClientThread clientThread;
+    @Inject
+    private EventBus eventBus;
+
     private static RuneliteContext instance = null;
 
-    private Client client;
-    private ClientThread clientThread;
-    private EventBus eventBus;
+    static void setInstance(RuneliteContext ctx) {
+        if (ctx.client == null) {
+            throw new IllegalStateException("client null");
+        }
+
+        if (ctx.clientThread == null) {
+            throw new IllegalStateException("clientThread null");
+        }
+
+        if (ctx.eventBus == null) {
+            throw new IllegalStateException("eventBus null");
+        }
+
+        instance = ctx;
+    }
 
     static RuneliteContext getInstance() {
         if (instance == null) {
